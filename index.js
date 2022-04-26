17cacb0f0dc54e2c4b673b3b9280d366415985cf
Sofia Wong <ssofia@ad.unc.edu>
2022-04-26 13:51:10 -0400

    more endpoint stuff

--- index.js
+++ index.js
@@ -86,6 +86,31 @@ const server = app.listen(port, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%', port));
 })
 
+//added
+app.use( (req, res, next) => {
+    // Your middleware goes here.
+    let logdata = {
+        remoteaddr: req.ip,
+        remoteuser: req.user,
+        time: Date.now(),
+        method: req.method,
+        url: req.url,
+        protocol: req.protocol,
+        httpversion: req.httpVersion,
+        status: res.statusCode,
+        referer: req.headers['referer'],
+        useragent: req.headers['user-agent']
+    };
+
+    console.log(logdata);
+
+    const prep = database.prepare('INSERT INTO accesslog (remoteaddr,remoteuser,time,method,url,protocol,httpversion,status,referer,useragent) VALUES (?,?,?,?,?,?,?,?,?,?)');
+
+    const run = prep.run(logdata.remoteaddr,logdata.remoteuser, logdata.time, logdata.method,logdata.url,logdata.protocol,logdata.httpversion, logdata.status,logdata.referer, logdata.useragent);
+    next();
+
+});
+
 // WHEN DEBUG == TRUE.
 if (debug) {
     app.use('/app', debug_router);
@@ -107,6 +132,91 @@ process.on('SIGTERM', () => {
     })
 })
 
+
+//my endpoints
+app.get('/app', (req, res)=>{
+    res.status(200).end('OK')
+    res.type('text/plain')
+  })
+  
+  app.get('/app/flip', (req, res) => {
+    res.status(200).json({ 'flip': coinFlip() })
+  })
+  
+  app.get('/app/flips/:number([0-9]{1,3})', (req, res) => {
+    const flips = coinFlips(req.params.number);
+      const numFlips = countFlips(flips);
+    res.status(200).json({'raw': flips, 'summary': numFlips})
+  });//one line
+  
+  app.get('/app/flip/call/heads', (req, res) => {
+    res.status(200).json({ 'message': flipACoin('heads')})
+  })
+  
+  app.get('/app/flip/call/tails', (req, res) => {
+    res.status(200).json({ 'message': flipACoin('tails')})
+  })
+  
+  //added endpoint:
+  if (args.debug) {
+      app.get('/app/log/access', (req, res) =>{
+          const stmt = db.prepare("Select * FROM accesslog").all();
+          res.status(200).json(stmt);
+      });
+      app.get("/app/error", (req, res) => {
+          throw new Error("Error Test Successful");
+      })
+  }
+  
+  app.use(function(req, res) {
+    res.status(404).end("Endpoint does not exist")
+    res.type("text/plain")
+  })
+
+  //done
+
+
+
+
+function coinFlip() {
+    var x = Math.round(Math.random());
+    if (x < 1) {return "heads";} else {return "tails";}
+  }
+
+  function coinFlips(flips) {
+    const flipArray = []
+    for(let i = 0; i<flips; i++){
+      flipArray[i] = coinFlip()
+    }
+    return flipArray
+  }
+  
+  
+  function countFlips(array) {
+    var h = 0;
+    var t = 0;
+    for(let i = 0; i<array.length; i++){
+      if(array[i]==='heads'){
+        h++;
+      }
+      else{
+        t++;
+      }
+    }
+    return {heads: h, tails: t};
+  }
+  
+  
+  function flipACoin(call) {
+    let flipCall = {call: call, flip: coinFlip(), result: ''};
+    if(flipCall.call === flipCall.flip){
+      flipCall.result = 'win';
+    } 
+    else{
+      flipCall.result = 'lose';
+    }
+    return flipCall;
+  }
 
 //example:
 app.post('/app/flip/coins/', (req, res, next) => {
