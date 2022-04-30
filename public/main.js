32e2bfc657b7b9cabbc1e35b696c378c912b6a12
Sofia Wong <ssofia@ad.unc.edu>
2022-04-30 15:06:05 -0400

    index.html

--- public/main.js
+++ public/main.js
@@ -41,8 +41,12 @@ function activeGuess() {
 const coin = document.getElementById("coin")
 // Add event listener for coin button
 coin.addEventListener("click", flipCoin)
-function flipCoin() {
-    fetch('http://localhost:5555/app/flip/', { mode: 'cors' })
+async function flipCoin() {
+    // Build up the endpoint URL
+    const endpoint = "app/flip/"
+    // DOM knows what the URI is so that we don't have to hard code it.
+    const url = document.baseURI+endpoint
+    await fetch (url)
         .then(function (response) {
             return response.json();
         })
@@ -55,6 +59,89 @@ function flipCoin() {
             // coin.disabled = true
         })
 }
+
+const coins = document.getElementById("coins");
+//function to flip multiple coins
+async function flipCoins(event) {
+        event.preventDefault();
+    // Build up the endpoint URL
+        const endpoint = "app/flip/coins/"
+        const url = document.baseURI+endpoint
+    // This extracts the data object from the form so we can run it through the FormData API
+        const formEvent = event.currentTarget
+    // Give the data to FormData and wait for a response or log an error to console.
+        try {
+            const formData = new FormData(formEvent);
+    // Hand the form data off to the function that is actually going to interact with the API.
+            const flips = await sendFlips({ url, formData });
+    // Process the response and manipulate some elements in div#multi.
+            console.log(flips);
+    // Present the summary information.
+            document.getElementById("heads").innerHTML = "Heads: "+flips.summary.heads;
+            document.getElementById("tails").innerHTML = "Tails: "+flips.summary.tails;
+    // This calls a function what will make a list of coin images based on the array of coin flip results.
+    // See below for coinList() function.
+        document.getElementById("coinlist").innerHTML = coinList(flips.raw);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+// Guess a coin flip by making a selection and pressing the button.
+// This uses a form in div#guesscoin with a selector to input the value to be sent to the API.
+const call = document.getElementById("call")
+
+
+coins.addEventListener("submit", flipCall)
+
+async function flipCall(event) {
+    // Prevent the default reload on event.
+        event.preventDefault();
+    // Build the url string.
+        const endpoint = "app/flip/call/"
+        const url = document.baseURI+endpoint
+    // Extract the data from the form.
+        const formEvent = event.currentTarget
+    // Give the data to FormData and wait for a response or log an error to console.
+        try {
+            const formData = new FormData(formEvent); 
+    // Hand the form data off to the function that is actually going to interact with the API.
+            const results = await sendFlips({ url, formData });
+    // Process the results.
+            console.log(results);
+    // Present the text results
+            document.getElementById("choice").innerHTML = "Guess: "+results.call;
+            document.getElementById("actual").innerHTML = "Actual: "+results.flip;
+            document.getElementById("results").innerHTML = "Result: "+results.result;
+    // Assemble a list containing images corresponding to the game results
+        document.getElementById("coingame").innerHTML = '<li><img src="assets/img/'+results.call+'.png" class="bigcoin" id="callcoin"></li><li><img src="assets/img/'+results.flip+'.png" class="bigcoin"></li><li><img src="assets/img/'+results.result+'.png" class="bigcoin"></li>';
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    // Create a data sender to sent POST request objects from FormData to send to the API using fetch()
+    async function sendFlips({ url, formData }) {
+    // Extract the form data from the FormData object
+        const plainFormData = Object.fromEntries(formData.entries());
+    // Turn the FormData into JSON
+        const formDataJson = JSON.stringify(plainFormData);
+    // Show the console what is going to be sent in the API message body
+        console.log(formDataJson);
+    // Set up the request object for fetch()
+        const options = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            },
+            body: formDataJson
+        };
+    // Send the request and wait for the response
+        const response = await fetch(url, options);
+    // Pass the response back to the event handler
+        return response.json()
+    }
+
+
 
 const flip_one_coin = document.getElementById("singlenav")
 // Add event listener for coin button
